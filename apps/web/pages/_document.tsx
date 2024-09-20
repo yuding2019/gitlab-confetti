ff1c16bede09e7621ba3c82f9246850834782374
yuding2019 <49802179+yuding2019@users.noreply.github.com>
2024-09-20 02:09:38 +0000

    feat: 添加微软bing seo meta

--- apps/web/pages/_document.tsx
+++ apps/web/pages/_document.tsx
@@ -11,6 +11,7 @@ export default function RootLayout() {
       <Head>
         <link rel="icon" href="./gitlab.png" />
         <meta name="google-site-verification" content="HRWXPoUq-hDGzLwQlK8QNFBIAWM9E7OluBbNQPopMjQ" />
+        <meta name="msvalidate.01" content="7B564FE7DF8FFBF367DD9F508778FAB0" />
       </Head>
       <title>GitLab Confetti 🎉</title>
       <body className="font-geist">
