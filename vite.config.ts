18facc12c48e04f043708af74c6a730f0283cb98
yuding2019 <49802179+yuding2019@users.noreply.github.com>
2024-09-21 11:03:12 +0000

    fix: 添加basepath环境变量

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react';
 
 const isDev = process.env.NODE_ENV === 'development';
 
+const defaultBasePath = isDev ? '/' : '/gitlab-confetti/';
+
+const basePath = process.env.BASE_PATH;
+
 export default defineConfig({
-  base: isDev ? '/' : '/gitlab-confetti/',
+  base: basePath || defaultBasePath,
   root: 'web',
   build: {
     outDir: 'out',
